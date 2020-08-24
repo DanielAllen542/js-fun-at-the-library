cebcf032086a88347fa9c108670862bc5ada50cb
Daniel Allen <danielallen542@gmail.com>
2020-08-23 19:47:21 -0600

    Added some shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,25 @@
+function shelfBook(bookObj, shelf) {
+ shelf.unshift(bookObj);
+ if (shelf.length > 3) {
+   shelf.shift();
+ }
+}
 
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++)
+  if (shelf[i].title === book)
+   shelf.splice(i, 1);
+}
+
+function listTitles(shelf) {
+  var bookTitles = []
+  for (var i = 0; shelf.length; i++ )
+  titleArray
+}
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
   // searchShelf: searchShelf
 };
